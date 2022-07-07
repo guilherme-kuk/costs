add61b1186070fcc0a3dd43921ef840e0392581d
Guilherme Kuk <guilhermekuk@gmail.com>
2022-07-07 01:44:40 -0300

    criado e configurado rotas do projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,7 @@
+export default function NewProject() {
+  return(
+    <div>
+      <h1>Novo Projeto</h1>
+    </div>
+  );
+};
