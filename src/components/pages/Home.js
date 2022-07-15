b40ea9755e49e64251d8b5c0830423a1ee974dd4
Guilherme Kuk <guilhermekuk@gmail.com>
2022-07-15 19:22:50 -0300

    feito estilização da Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,15 @@
+import savings from '../../img/savings.svg';
+import LinkButton from '../layout/LinkButton';
+
 export default function Home() {
   return(
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className="home_container">
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      {/* criar novo projeto*/}
+      {/* router */}
+      <LinkButton to="/newproject" text="Criar Projeto" />
+      <img src={savings} alt="Costs" />
+    </section>
   );
 };
