199ca2a2d0e586c22da0e0feca4bfe6bd7b29a8f
Guilherme Kuk <guilhermekuk@gmail.com>
2022-07-22 23:12:43 -0300

    iniciado NewProject e também adicionado component Projectform.js

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,11 @@
+import ProjectForm from "../project/ProjectForm";
+
 export default function NewProject() {
   return(
-    <div>
-      <h1>Novo Projeto</h1>
+    <div className="newproject_container">
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços.</p>
+      <ProjectForm />
     </div>
   );
 };
