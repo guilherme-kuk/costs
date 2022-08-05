7c5a3bfc4d742fc3ef5f7296869820361ef60c81
Guilherme Kuk <guilhermekuk@gmail.com>
2022-08-05 20:54:11 -0300

    utilizado fetch api para pegar categories do db.json e adicionado options

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -4,6 +4,12 @@ export default function Select({text, name, options, handleOnChange, value}){
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
         <option>Selecione uma opção</option>
+        {/* imprimindo as options */}
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   )
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,8 +1,33 @@
+// importando hooks
+import { useState, useEffect } from "react";
+// 
+
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
 export default function ProjectForm({ btnText }) {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+     // request de GET
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      // tipo da comunicação
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    // transformar e retornar a resposta em json
+    .then((resp) => resp.json())
+    // adicinar os dados no hook
+    .then((data) => {
+      setCategories(data)
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
     <form className="form">
       <Input 
@@ -19,7 +44,7 @@ export default function ProjectForm({ btnText }) {
         placeholder="Insira o orçamento total" 
       />
 
-      <Select name="category_id" text="Selecione a categoria"/>
+      <Select name="category_id" text="Selecione a categoria" options={categories}/>
 
       {/* btnText - reaproveitamento vindo do component NewProject */}
       <SubmitButton text={btnText}/>
